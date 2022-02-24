de896c0c5d4f304d8821a28d66149a1754d5b330
Rohit gupta <gupatrohit2490>
2022-02-24 19:04:39 +0530

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sum10nnm
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            int sum=0;
+            for (i = 1; i <= 10; i++)
+            {
+                sum = sum + i;
+                Console.WriteLine("{0}",i);
+            }
+            Console.WriteLine("sum is{0}", sum);
+            Console.ReadLine();
+
+        }
+    }
+}
