cc5084155994366005b6df8ce3148ffd54e78fc5
Rohit gupta <99723140+guptarohit24@users.noreply.github.com>
2022-02-27 16:40:17 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,20 +4,44 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace sum10nnm
+namespace publicmethod
+
 {
+    class myclass1
+    {
+        public string name;
+        public int age;
+        public int id;
+        public string school;
+        public void getdetail(string name, int age ,int  id ,string school)
+        {
+            this.name = name;
+            this.age = age;
+            this.id = id;
+            this.school = school;
+            
+            Console.WriteLine("name = {0}  age = {1}  id = {2}  school = {3}",this.name,this.age ,this.id,this.school);
+            Console.ReadLine();
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
-            int i;
-            int sum=0;
-            for (i = 1; i <= 10; i++)
-            {
-                sum = sum + i;
-                Console.WriteLine("{0}",i);
-            }
-            Console.WriteLine("sum is{0}", sum);
+              myclass1 obj1 = new myclass1();
+           
+            Console.WriteLine("enter the  name");
+            string name = Console.ReadLine();
+            Console.WriteLine("enter the age ");
+            int age = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter the school id");
+            int id = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter the school nmae");
+            string school = Console.ReadLine();
+            Console.WriteLine(name, age, id, school);
+            
+
+            obj1.getdetail("rohit",21,212,"pms");
             Console.ReadLine();
 
         }
